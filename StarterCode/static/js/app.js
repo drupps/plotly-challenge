a293aff7ab6d6f8a80798c0db05d3de93689555f
Dave Ruppel <drupps@gmail.com>
2020-11-13 15:40:23 -0800

    updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -3,13 +3,19 @@
 // var ynames = names
 // var xotu = otu
 
+// d3.json("../StarterCode/samples.json", function(data){
+//     console.log(data);
+// });
 
 
 
+var names = [];
+var id = [];
+var ethnicity = [];
+var gender = [];
+var age = [];
+var location = [];
+var bbtype = [];
+var wfreq = [];
 
 
-
-
-d3.json("../StarterCode/samples.json", function(data){
-    console.log(data);
-});
