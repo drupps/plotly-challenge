af5c7ded1cd9a0d48af9d3fedc5e6cf61f3fb7b6
Dave Ruppel <drupps@gmail.com>
2020-11-13 14:44:55 -0800

    basic updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -1,4 +1,15 @@
 //reading in samples.json
-d3.json("../StarterCode/samples.json", function(data){
-    console.log(data);
-});
+
+var ynames = names.samples.json
+var xotu = otu.samples.json
+
+
+
+
+
+
+
+
+// d3.json("../StarterCode/samples.json", function(data){
+//     console.log(data);
+// });
