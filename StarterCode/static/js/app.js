650429aa47be229ef6512b1c5877969999e67c15
Dave Ruppel <drupps@gmail.com>
2020-11-16 19:52:04 -0800

    not working plus updates

--- StarterCode/static/js/app.js
+++ StarterCode/static/js/app.js
@@ -17,35 +17,24 @@ d3.json("samples.json").then(data => {
     //settting up the values of the top ten otus
     var values = data.samples[0].sample_values.slice(0, 10);
 
-    //setting up the id's for the top ten
-    var ids = data.samples[0].out_ids.slice(0, 10).map(d => );
+    //setting up the id's for the top ten and call it OTU!
+    var ids = data.samples[0].otu_ids.slice(0, 10).map(d => 'OTU ${d}');
 
     //setting up the labels for the top ten
-    var labels = data.samples[0].out_labels.slice(0, 10);
+    var labels = data.samples[0].otu_labels.slice(0, 10);
         
-    
-// //start of the dropdown menu
-//     var dropdownMenu = d3.select("#selDataset");
+    //Create the Traces
+    var trace1 = {
+    x: values,
+    y: ids,
+    type: "bar",
+    name: "Top 10 OTUs",
+    text: labels,
+    };
 
-//      //Create the Traces
-//     var trace1 = {
-//       x: data.id,
-//       y: data.otu_ids,
-//       type: "bar",
-//       name: "Top 10 OTUs",
-//     //   boxpoints: "all"
-//     };
-  
-//     // Create the data array for the plot
-//     var data = [trace1];
-  
-//     // Define the plot layout
-//     var layout = {
-//       title: "Prevalence of OTUs in bellybutton's",
-//       xaxis: { title: "Sample Values" },
-//       yaxis: { title: "OTU ID's" }
-//     };
-  
-//     // Plot the chart to a div tag with id "plot"
-//     Plotly.newPlot("bar", data, layout);
-  });
+//setting up trace1 to the plotBar var
+var plotBar = [trace1]
+
+//actual plotting of the bar
+plotly.newPlot('bar', plotBar)
+});
